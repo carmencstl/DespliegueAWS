f897c9aaa0927ca9cbcef8d305cd17a29b497ef2
carmencstl <ccastillogaitan12@gmail.com>
2026-02-09 22:42:36 +0100

    prueba 8

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   required_version = ">= 1.2"
   
   backend "s3" {
-    bucket = "bucket-terraform-github-actions"
+    bucket = "deploy-bucket-clase"
     key    = "terraform/state.tfstate"
     region = "us-east-1"
   }
