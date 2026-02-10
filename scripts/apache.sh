b0ad07719933e5da2aab83e67904a0cc8363e70c
Carmen <ccastillogaitan12@gmail.com>
2026-02-10 10:31:45 +0100

    prueba 15

--- scripts/apache.sh
+++ scripts/apache.sh
@@ -1,23 +1,23 @@
 #!/bin/bash -xe
-exec > /tmp/userdata.log 2>&1   
+exec > /tmp/userdata.log 2>&1
 
-apt update -y                   
-apt upgrade -y                  
-apt install -y apache2         
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y apache2
 
-a2dismod autoindex
-a2dismod status
-a2dismod userdir
-a2dismod info
-           
+a2dismod autoindex || true
+a2dismod status || true
+a2dismod userdir || true
+a2dismod info || true
+
 systemctl restart apache2
 
-apt install -y ruby-full wget
+apt-get install -y ruby-full wget
 
-cd /home/ubuntu
+cd /tmp
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
-./install auto
+sudo ./install auto
 
 systemctl start codedeploy-agent
 systemctl enable codedeploy-agent
